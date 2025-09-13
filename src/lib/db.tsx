df58962bc0fb851769ac8ef8d58ea8472e8c9de4
shukri0c <shukriamahdi05@gmail.com>
2025-09-13 14:59:08 +0100

    fix login issues

--- src/lib/db.tsx
+++ src/lib/db.tsx
@@ -5,6 +5,14 @@ const globalForPrisma = global as typeof globalThis & {
 };
 
 export const db =
-  globalForPrisma.prisma ?? new PrismaClient({ log: ["query"] });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + "&connection_limit=20&pool_timeout=60",
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
